ec19b84f7da2a485f4d1402ed785f9fec58603dc
Alex Ford <alex96ford19@gmail.com>
2024-10-23 21:35:52 +0100

    Add verbs and body page

---
+++ assets/js/verbs.js
@@ -0,0 +1,104 @@
+window.verbs = {
+    "tener": {
+        "present": {
+            "yo": { "es": "tengo", "en": "I have" },
+            "tú": { "es": "tienes", "en": "you have" },
+            "él/ella/usted": { "es": "tiene", "en": "he/she/you (formal) has/have" },
+            "nosotros/nosotras": { "es": "tenemos", "en": "we have" },
+            "vosotros/vosotras": { "es": "tenéis", "en": "you all have" },
+            "ellos/ellas/ustedes": { "es": "tienen", "en": "they/you all have" }
+        },
+        "preterite": {
+            "yo": { "es": "tuve", "en": "I had" },
+            "tú": { "es": "tuviste", "en": "you had" },
+            "él/ella/usted": { "es": "tuvo", "en": "he/she/you (formal) had" },
+            "nosotros/nosotras": { "es": "tuvimos", "en": "we had" },
+            "vosotros/vosotras": { "es": "tuvisteis", "en": "you all had" },
+            "ellos/ellas/ustedes": { "es": "tuvieron", "en": "they/you all had" }
+        },
+        "imperfect": {
+            "yo": { "es": "tenía", "en": "I used to have" },
+            "tú": { "es": "tenías", "en": "you used to have" },
+            "él/ella/usted": { "es": "tenía", "en": "he/she/you (formal) used to have" },
+            "nosotros/nosotras": { "es": "teníamos", "en": "we used to have" },
+            "vosotros/vosotras": { "es": "teníais", "en": "you all used to have" },
+            "ellos/ellas/ustedes": { "es": "tenían", "en": "they/you all used to have" }
+        },
+        "future": {
+            "yo": { "es": "tendré", "en": "I will have" },
+            "tú": { "es": "tendrás", "en": "you will have" },
+            "él/ella/usted": { "es": "tendrá", "en": "he/she/you (formal) will have" },
+            "nosotros/nosotras": { "es": "tendremos", "en": "we will have" },
+            "vosotros/vosotras": { "es": "tendréis", "en": "you all will have" },
+            "ellos/ellas/ustedes": { "es": "tendrán", "en": "they/you all will have" }
+        }
+    },
+    "hacer": {
+        "present": {
+            "yo": { "es": "hago", "en": "I do" },
+            "tú": { "es": "haces", "en": "you do" },
+            "él/ella/usted": { "es": "hace", "en": "he/she/you (formal) does" },
+            "nosotros/nosotras": { "es": "hacemos", "en": "we do" },
+            "vosotros/vosotras": { "es": "hacéis", "en": "you all do" },
+            "ellos/ellas/ustedes": { "es": "hacen", "en": "they/you all do" }
+        },
+        "preterite": {
+            "yo": { "es": "hice", "en": "I did" },
+            "tú": { "es": "hiciste", "en": "you did" },
+            "él/ella/usted": { "es": "hizo", "en": "he/she/you (formal) did" },
+            "nosotros/nosotras": { "es": "hicimos", "en": "we did" },
+            "vosotros/vosotras": { "es": "hicisteis", "en": "you all did" },
+            "ellos/ellas/ustedes": { "es": "hicieron", "en": "they/you all did" }
+        },
+        "imperfect": {
+            "yo": { "es": "hacía", "en": "I used to do" },
+            "tú": { "es": "hacías", "en": "you used to do" },
+            "él/ella/usted": { "es": "hacía", "en": "he/she/you (formal) used to do" },
+            "nosotros/nosotras": { "es": "hacíamos", "en": "we used to do" },
+            "vosotros/vosotras": { "es": "hacíais", "en": "you all used to do" },
+            "ellos/ellas/ustedes": { "es": "hacían", "en": "they/you all used to do" }
+        },
+        "future": {
+            "yo": { "es": "haré", "en": "I will do" },
+            "tú": { "es": "harás", "en": "you will do" },
+            "él/ella/usted": { "es": "hará", "en": "he/she/you (formal) will do" },
+            "nosotros/nosotras": { "es": "haremos", "en": "we will do" },
+            "vosotros/vosotras": { "es": "haréis", "en": "you all will do" },
+            "ellos/ellas/ustedes": { "es": "harán", "en": "they/you all will do" }
+        }
+    },
+    "traer": {
+        "present": {
+            "yo": { "es": "traigo", "en": "I bring" },
+            "tú": { "es": "traes", "en": "you bring" },
+            "él/ella/usted": { "es": "trae", "en": "he/she/you (formal) brings" },
+            "nosotros/nosotras": { "es": "traemos", "en": "we bring" },
+            "vosotros/vosotras": { "es": "traéis", "en": "you all bring" },
+            "ellos/ellas/ustedes": { "es": "traen", "en": "they/you all bring" }
+        },
+        "preterite": {
+            "yo": { "es": "traje", "en": "I brought" },
+            "tú": { "es": "trajiste", "en": "you brought" },
+            "él/ella/usted": { "es": "trajo", "en": "he/she/you (formal) brought" },
+            "nosotros/nosotras": { "es": "trajimos", "en": "we brought" },
+            "vosotros/vosotras": { "es": "trajisteis", "en": "you all brought" },
+            "ellos/ellas/ustedes": { "es": "trajeron", "en": "they/you all brought" }
+        },
+        "imperfect": {
+            "yo": { "es": "traía", "en": "I used to bring" },
+            "tú": { "es": "traías", "en": "you used to bring" },
+            "él/ella/usted": { "es": "traía", "en": "he/she/you (formal) used to bring" },
+            "nosotros/nosotras": { "es": "traíamos", "en": "we used to bring" },
+            "vosotros/vosotras": { "es": "traíais", "en": "you all used to bring" },
+            "ellos/ellas/ustedes": { "es": "traían", "en": "they/you all used to bring" }
+        },
+        "future": {
+            "yo": { "es": "traeré", "en": "I will bring" },
+            "tú": { "es": "traerás", "en": "you will bring" },
+            "él/ella/usted": { "es": "traerá", "en": "he/she/you (formal) will bring" },
+            "nosotros/nosotras": { "es": "traeremos", "en": "we will bring" },
+            "vosotros/vosotras": { "es": "traeréis", "en": "you all will bring" },
+            "ellos/ellas/ustedes": { "es": "traerán", "en": "they/you all will bring" }
+        }
+    }
+}

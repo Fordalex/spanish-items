65c6ab4548fe3ccd688db60406cb3057f1695073
Alex Ford <alex96ford19@gmail.com>
2024-10-23 12:51:49 +0100

    Add house

--- assets/js/overlay.js
+++ assets/js/overlay.js
@@ -8,16 +8,16 @@ document.addEventListener('DOMContentLoaded', function () {
         let item = document.createElement('div');
         item.classList.add('item');
         item.classList.add(word.english.toLowerCase().replace(/ /g, '-'));
+        item.dataset.bsToggle = 'modal';
+        item.dataset.bsTarget = '#modal';
 
         let sentenceCount = word.sentences ? word.sentences.length : 0;
 
         let translation = document.createElement('div');
         translation.classList.add('translation');
         translation.innerHTML = `${word.spanish} (${word.english}) - ${sentenceCount}`;
-        translation.dataset.bsToggle = 'modal';
-        translation.dataset.bsTarget = '#modal';
 
-        translation.addEventListener('click', function () {
+        item.addEventListener('click', function () {
             modalTitle.textContent = word.spanish;
 
             if (word.sentences) {
